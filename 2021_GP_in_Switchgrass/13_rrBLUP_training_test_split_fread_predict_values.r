79ea826a655dbac5d6dc6c89f2f1d4840b77d1b7
Peipei Wang <peipeiwang6@gmail.com>
2021-12-23 22:17:08 -0500

    Update 13_rrBLUP_training_test_split_fread_predict_values.r
    Have Modified the way how the test info was included in the cvs file. Won't change the results if the phenotype csv file contains both training and test instances.

--- 2021_GP_in_Switchgrass/13_rrBLUP_training_test_split_fread_predict_values.r
+++ 2021_GP_in_Switchgrass/13_rrBLUP_training_test_split_fread_predict_values.r
@@ -39,10 +39,15 @@ if(file.size(X_file) > 10*1024*1024){
 
 
 cvs <- read.csv(cvs_file, row.names=1)
-cvs_all <- merge(Y,cvs,by="row.names",all.x=TRUE)
-rownames(cvs_all) <- cvs_all$Row.names
-cvs_all <- cvs_all[,(dim(Y)[2]+2):ncol(cvs_all)]
-cvs_all[is.na(cvs_all)] = 0
+test_matrix <- matrix(nrow=length(Test),ncol=number,0)
+rownames(test_matrix) <- Test
+colnames(test_matrix) <- colnames(cvs)
+cvs_all <- rbind(cvs,test_matrix)
+
+#cvs_all <- merge(Y,cvs,by="row.names",all.x=TRUE)
+#rownames(cvs_all) <- cvs_all$Row.names
+#cvs_all <- cvs_all[,(dim(Y)[2]+2):ncol(cvs_all)]
+#cvs_all[is.na(cvs_all)] = 0
 
 # make sure X and Y have the same order of rows as cvs_all
 X <- X[rownames(cvs_all),]
